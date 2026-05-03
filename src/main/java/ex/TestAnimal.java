d6ff573e5535f4c651eb5672e79dcce0860f50b7
twixu819 <savelamaksim011@gmail.com>
2026-05-04 01:22:23 +0300

    Add main method to TestAnimal class

--- src/main/java/ex/TestAnimal.java
+++ src/main/java/ex/TestAnimal.java
@@ -1,4 +1,14 @@
 package ex;
 
 public class TestAnimal {
+    public static void main(String[] args) {
+        Chupacabra chupacabra = new Chupacabra("Yushka", 50, "Selo");
+
+        chupacabra.eat();
+        chupacabra.speak();
+        chupacabra.hide();
+        chupacabra.hunt();
+
+        System.out.println(chupacabra.toString());
+    }
 }
