498492bee5ad719f20aa99e851099a71c0f31975
twixu819 <savelamaksim011@gmail.com>
2026-05-04 01:21:36 +0300

    Extend Chupacabra class with region and methods

--- src/main/java/ex/Chupacabra.java
+++ src/main/java/ex/Chupacabra.java
@@ -1,4 +1,17 @@
 package ex;
 
-public class Chupacabra {
+public class Chupacabra extends Cryptid {
+    private String region;
+
+    public Chupacabra(String name, int weight, String region) {
+        this.region = region;
+    }
+
+    public Chupacabra() {
+        this.region = "Ukrainian selo";
+    }
+
+    public void hunt() {
+        System.out.println("Chupacabra hunts in the region " + region);
+    }
 }
