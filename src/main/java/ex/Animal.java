a5fe934e28012743979e413d4776b7f5230ab9ae
twixu819 <savelamaksim011@gmail.com>
2026-05-04 01:21:10 +0300

    Update Animal.java

--- src/main/java/ex/Animal.java
+++ src/main/java/ex/Animal.java
@@ -1,17 +1,23 @@
-package org.example;
+package ex;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Animal {
+    private String name;
+    private int weight;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    public Animal() {
+        this.name = "Kokoska";
+        this.weight = 48;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    public void speak() {
+        System.out.println(name + " makes a sound");
+    }
+
+    public String toString() {
+        return "Animal: " + name + "   Weight: " + weight;
     }
 }
